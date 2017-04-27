b8a906dd2050a3452e4bc3abeb11a42a0bdb17b1
Karl Shifflett <kdawg1406@gmail.com>
2017-04-27 13:45:00 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Tame Visual Studio Editor Tool Tips
+
+<!-- Update the VS Gallery link after you upload the VSIX-->
+Download this extension from the [VS Gallery](https://marketplace.visualstudio.com/vsgallery/097521bb-c432-4436-ba9f-137dcb03d3be)
+
+---------------------------------------
+Tames the Visual Studio Code Editor Tool Tips by not showing them unless the CTRL key is depressed and the user hovers over the object.  
+
+For cloning and building this project yourself, make sure to install 
+[Extensibility Tools 2015](https://visualstudiogallery.msdn.microsoft.com/ab39a092-1343-46e2-b0f1-6a3f91155aa6)
+extension for Visual Studio which enables some features
+used by this project.
+
+## License
+[Apache 2.0](LICENSE)
---
+++ TameVisualStudioEditorToolTips/TameQuickInfo.cs
@@ -0,0 +1,37 @@
+﻿namespace TameVisualStudioEditorToolTips {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using System.Windows.Input;
+    using Microsoft.VisualStudio.Language.Intellisense;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Utilities;
+
+    [Export(typeof(IQuickInfoSourceProvider))]
+    [Name("Tame Quick Info")]
+    [Order(Before = "Default Quick Info Presenter")]
+    [ContentType("CSharp")]
+    [ContentType("XAML")]
+    [ContentType("XML")]
+    internal class TameQuickInfo : IQuickInfoSourceProvider {
+
+        public IQuickInfoSource TryCreateQuickInfoSource(ITextBuffer textBuffer) {
+            return new CancelingQuickInfoSource();
+        }
+
+    }
+
+    internal class CancelingQuickInfoSource : IQuickInfoSource {
+
+        public void AugmentQuickInfoSession(IQuickInfoSession session, IList<Object> quickInfoContent, out ITrackingSpan applicableToSpan) {
+            if (!Keyboard.IsKeyDown(Key.LeftCtrl) && !Keyboard.IsKeyDown(Key.RightCtrl)) {
+                session.Dismiss();
+            }
+            applicableToSpan = null;
+        }
+
+        public void Dispose() {
+        }
+
+    }
+}
